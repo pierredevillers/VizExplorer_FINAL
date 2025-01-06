f21844c9fb804db93bf6d28842010540876143cf
Pierre Devillers <pierre.devillers@unil.ch>
2025-01-06 22:34:48 +0100

    Update for online deployment of the app

---
+++ VizExplorer_FINAL/config.py
@@ -0,0 +1,11 @@
+POSTGRESQL_CREDENTIALS = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "swiss_private_bank",
+    "user": "postgres",
+    "password": "123456789",
+}
+
+DATABASE_URL = "postgresql://swiss_private_bank_owner:p3g7qazZiGle@ep-snowy-tooth-a27ji8ct.eu-central-1.aws.neon.tech/swiss_private_bank?sslmode=require"
+
+VANNA_API_KEY = "be920fe18e6c4a3fa6bf9436d6113657"
